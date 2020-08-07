942958312ca5efb23f7096e78d8cb812502d77af
lzj <lizhijian@jimilab.com>
2020-08-07 12:01:27 +0800

    1.5.3

--- JimiVideoPlayer.framework/Headers/Monitor.h
+++ JimiVideoPlayer.framework/Headers/Monitor.h
@@ -10,4 +10,15 @@
 
 @interface Monitor : UIImageView
 
+/// 显示图片
+/// @param image UIImage
+- (void)displayImage:(UIImage *)image;
+
+/// 加载RGB24画面数据
+/// @param rgbData rgb24数据
+/// @param width 宽
+/// @param height 高
+- (void)displayRgb24Data:(const u_char *)rgbData videoWidth:(NSInteger)width videoHeight:(NSInteger)height;
+
+
 @end
